1a80b69bc705da0acb1bd389eaa583743f770f5c
SmileYuhao <prayer_zhao@outlook.com>
2017-01-12 00:13:23 +0100

    Init commit

---
+++ concept_dirft/adwin.py
@@ -0,0 +1,18 @@
+"""Adaptive Sliding Window"""
+
+
+class Adwin():
+    def __init__(self):
+        pass
+
+    def set_input(self, value):
+        self.insert_element(value)
+
+    def insert_element(self, value):
+        pass
+
+    def delete_element(self):
+        pass
+
+    def compress_buckets(self):
+        pass
