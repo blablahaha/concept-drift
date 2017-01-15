6c3374440b25a411c7f11c7762baa059ebdbf669
SmileYuhao <prayer_zhao@outlook.com>
2017-01-15 20:04:29 +0100

    Update comment

--- concept_dirft/adwin_list_item.py
+++ concept_dirft/adwin_list_item.py
@@ -18,14 +18,14 @@ class AdwinListItem:
         self.bucket_variance = np.zeros(self.max_buckets + 1)
 
     def insert_bucket(self, value, variance):
-        """Insert a new element into bucket
+        """Insert a new bucket
         """
         self.bucket_total[self.bucket_size_row] = value
         self.bucket_variance[self.bucket_size_row] = variance
         self.bucket_size_row += 1
 
     def compress_buckets_row(self, number_deleted):
-        """Remove the elements which have been already merged into another bucket
+        """Remove the bucket
         """
         self.bucket_total[:self.max_buckets - number_deleted + 1] = self.bucket_total[number_deleted:]
         self.bucket_total[self.max_buckets - number_deleted + 1:] = np.zeros(number_deleted)
