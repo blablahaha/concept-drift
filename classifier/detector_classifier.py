7b3ec13f6d8061acac4d69630cd0a67b43623705
SmileYuhao <prayer_zhao@outlook.com>
2017-02-10 23:17:58 +0100

    Update detector classifier

--- classifier/detector_classifier.py
+++ classifier/detector_classifier.py
@@ -1,13 +1,17 @@
 """"Classifier that replaces the current classifier with a new one when a change is detected in accuracy.
 """
-from sklearn.metrics import accuracy_score
 from sklearn import clone
+from sklearn.base import BaseEstimator
+from sklearn.metrics import accuracy_score
 
 
-class DetectorClassifier:
-    def __init__(self, clf, detection_method):
+class DetectorClassifier(BaseEstimator):
+    def __init__(self, clf, detection_method, classes):
+        if not hasattr(clf, "partial_fit"):
+            raise TypeError("Choose incremental classifier")
         self.clf = clf
         self.detection_method = detection_method
+        self.classes = classes
         self.change_detected = 0
 
     def fit(self, X, y):
@@ -19,7 +23,9 @@ class DetectorClassifier:
         if self.detection_method.set_input(accuracy_score(pre_y, y)):
             self.change_detected += 1
             self.clf = clone(self.clf)
-        self.clf.fit(X, y)
+            self.clf.partial_fit(X, y, classes=self.classes)
+        else:
+            self.clf.partial_fit(X, y)
 
     def predict(self, X):
         return self.clf.predict(X)
